ab8be25e591e8cecb9088b5cff2afa150d2b11b8
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-10-17 20:25:38 +0700

    Implement upload avatar image feature, dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,12 @@ import { Header } from "./header/header.jsx";
 import { Outlet, useLoaderData } from "react-router";
 
 function App() {
-  // const [token, setToken] = useState(useContext(UserContext));
   const loaderData = useLoaderData();
   return (
     <>
-      {/* <userContext value={{ token, setToken }}> */}
-      {/* <Header /> */}
-      <div className="flex flex-col flex-col-reverse md:flex-row h-full">
+      <div className="flex flex-col-reverse md:flex-row h-full dark:bg-slate-900">
         <Header loaderData={loaderData}></Header>
         <Outlet></Outlet>
-        {/* </userContext> */}
       </div>
     </>
   );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,38 +1,38 @@
-import { useContext, useEffect, useState } from "react";
-// import { userContext } from "../../Context";
+import { useRef, useState } from "react";
+
 import { useLoaderData } from "react-router";
-import { Link } from "react-router";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_PROJECT_URL,
+  import.meta.env.VITE_SUPABASE_API_KEY
+);
 
 const Profile = () => {
-  const [user, setUser] = useState({});
   const [isUpdate, setIsupdate] = useState(false);
   const [result, setResult] = useState("");
   const [validations, setValidations] = useState([]);
   const loaderData = useLoaderData();
-
-  // const auth = useContext(userContext);
-
-  // useEffect(() => {
-  //   async function fetchUser() {
-  //     const id = localStorage.getItem("userId");
-  //     const response = await fetch(`http://localhost:3000/users/${id}`, {
-  //       method: "GET",
-
-  //       headers: {
-  //         "Content-type": "application/json",
-  //         Authorization: `bearer ${auth.token}`,
-  //       },
-  //     });
-  //     const result = await response.json();
-  //     setUser(result.user);
-  //   }
-  //   fetchUser();
-  // }, [auth.token]);
+  const avatarRef = useRef(null);
 
   async function handleUpdate(formData) {
-    const name = formData.get("fullname");
     const password = formData.get("password");
     const repeatPassword = formData.get("repeat_password");
+    const uploadeAvatar = formData.get("uploaded_avatar");
+
+    if (uploadeAvatar) {
+      const { data, error } = await supabase.storage
+        .from("avatars")
+        .upload(`${loaderData.username}/${uploadeAvatar.name}`, uploadeAvatar);
+
+      if (data) {
+        console.log(data);
+      } else {
+        console.log(error);
+        return;
+      }
+      formData.append("avatarPath", data.fullPath);
+    }
 
     if (password !== repeatPassword) {
       setValidations([
@@ -44,14 +44,16 @@ const Profile = () => {
       setValidations([]);
     }
 
-    const response = await fetch(`http://localhost:3000/users/${user.id}`, {
-      method: "PUT",
-      body: JSON.stringify({ name, password }),
-      headers: {
-        "Content-type": "application/json",
-        Authorization: `bearer ${loaderData.token}`,
-      },
-    });
+    const response = await fetch(
+      `http://localhost:3000/users/${loaderData.id}`,
+      {
+        method: "PUT",
+        body: formData,
+        headers: {
+          Authorization: `bearer ${loaderData.token}`,
+        },
+      }
+    );
     if (response.ok) {
       setIsupdate(false);
       setResult("You updated successfully!");
@@ -63,160 +65,196 @@ const Profile = () => {
     else setIsupdate(false);
   }
 
+  async function handleUpload(e) {
+    const avatarFile = e.target.files[0];
+    if (!avatarFile.type.startsWith("image/")) {
+      console.log("The file uploaded is not image!");
+      return;
+    }
+
+    avatarRef.current.classList.add("obj");
+    avatarRef.current.file = avatarFile;
+
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      avatarRef.current.src = e.target.result;
+    };
+
+    reader.readAsDataURL(avatarFile);
+  }
+
   return (
-    <>
-      <div className="grid grid-cols-[50px_1fr]">
-        <div className="col-start-1 flex flex-col justify-between">
-          <Link to="/">Chat</Link>
-          <Link className="col-start-1" to="/profile">
-            <svg
-              className="w-[48px] h-[48px] text-gray-800"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12 20a7.966 7.966 0 0 1-5.002-1.756l.002.001v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10c0 5.5-4.44 9.963-9.932 10h-.138C6.438 21.962 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </Link>
-        </div>
-        <div className="col-start-2">
-          <p>Profile</p>
-          <div className="md:grid grid-cols-3">
-            <form
-              action={handleUpdate}
-              className="flex flex-col bg-white col-span-1"
-            >
-              <div className="flex flex-col">
-                <label htmlFor="username" className="text-zinc-500">
-                  Username:
-                </label>
-                <input
-                  type="text"
-                  name="username"
-                  id="username"
-                  className="p-1.5 w-full"
-                  defaultValue={loaderData.username}
-                  required
-                />
-              </div>
-              <div className="flex flex-col mt-3">
-                <label htmlFor="password" className="text-zinc-500">
-                  Password:
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  className="p-1.5 w-full"
-                  required
-                  minLength={8}
-                  maxLength={30}
-                  defaultValue=""
-                />
-              </div>
-              <div className="flex flex-col mt-3">
-                <label htmlFor="password" className="text-zinc-500">
-                  Repeat Password:
-                </label>
-                <input
-                  type="password"
-                  name="repeat_password"
-                  id="repeat_password"
-                  className="p-1.5 w-full"
-                  required
-                  minLength={8}
-                  maxLength={30}
-                  defaultValue=""
-                />
-              </div>
-              <div className="flex flex-col mt-3">
-                <label htmlFor="password" className="text-zinc-500">
-                  Fullname:
-                </label>
-                <input
-                  type="text"
-                  name="fullname"
-                  id="fullname"
-                  className="p-1.5 w-full"
-                  required
-                  defaultValue={loaderData.name}
-                />
-              </div>
-              <div className="flex flex-col mt-3">
-                <label htmlFor="password" className="text-zinc-500">
-                  About:
-                </label>
-                <textarea name="about">{loaderData.about}</textarea>
-              </div>
-              <div className="flex flex-col mt-3">
-                <label htmlFor="password" className="text-zinc-500">
-                  Phone:
-                </label>
-                <input
-                  type="tel"
-                  name="phone"
-                  defaultValue={loaderData.phone}
-                />
-              </div>
-              <div className="flex gap-2.5">
-                {isUpdate ? (
-                  <button
-                    type="submit"
-                    className="mt-5 bg-blue-700 text-white flex-1"
-                  >
-                    Update
-                  </button>
-                ) : (
-                  <button
-                    type="submit"
-                    className="mt-5 text-white flex-1 bg-gray-500"
-                    disabled
-                  >
-                    Update
-                  </button>
-                )}
-                {isUpdate ? (
-                  <button
-                    type="button"
-                    className={"mt-5 bg-blue-700 text-white flex-1"}
-                    onClick={handleEdit}
-                  >
-                    Cancel
-                  </button>
-                ) : (
-                  <button
-                    type="button"
-                    className={"mt-5 bg-blue-700 text-white flex-1"}
-                    onClick={handleEdit}
-                  >
-                    Edit
-                  </button>
-                )}
-              </div>
-              {validations.length > 0 && (
-                <ul>
-                  {validations.map((validation, index) => {
-                    return (
-                      <li key={index} className="text-red-500 ">
-                        {validation}
-                      </li>
-                    );
-                  })}
-                </ul>
+    <div className="md:grid md:grid-cols-3 flex-1">
+      <div className="p-8 flex flex-col gap-y-4">
+        <p className="dark:text-gray-50 text-2xl">Profile</p>
+        <form
+          action={handleUpdate}
+          className="flex flex-col col-span-1 profile__form gap-y-8"
+        >
+          <div className="flex justify-center">
+            <label htmlFor="uploaded-avatar">
+              {loaderData.avatarPath ? (
+                <img
+                  className="w-36"
+                  src={`https://bkudoqbqykfhbgcxfelw.supabase.co/storage/v1/object/public/${loaderData.avatarPath}`}
+                  ref={avatarRef}
+                ></img>
+              ) : (
+                <svg
+                  className="w-36 h-36 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 20a7.966 7.966 0 0 1-5.002-1.756l.002.001v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10c0 5.5-4.44 9.963-9.932 10h-.138C6.438 21.962 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
               )}
-              {result ? <p>{result}</p> : ""}
-            </form>
+            </label>
+            <input
+              type="file"
+              name="uploaded_avatar"
+              id="uploaded-avatar"
+              className="dark:text-gray-50 hidden"
+              onChange={handleUpload}
+            />
           </div>
-        </div>
+          <div className="flex flex-col">
+            <label htmlFor="username" className="text-zinc-500">
+              Username:
+            </label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              className="p-1.5 w-full dark:text-gray-50"
+              defaultValue={loaderData.username}
+              required
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password" className="text-zinc-500">
+              Password:
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="p-1.5 w-full dark:text-gray-50"
+              required
+              minLength={8}
+              maxLength={30}
+              defaultValue=""
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password" className="text-zinc-500">
+              Repeat Password:
+            </label>
+            <input
+              type="password"
+              name="repeat_password"
+              id="repeat_password"
+              className="p-1.5 w-full dark:text-gray-50"
+              required
+              minLength={8}
+              maxLength={30}
+              defaultValue=""
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password" className="text-zinc-500">
+              Fullname:
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="fullname"
+              className="p-1.5 w-full dark:text-gray-50"
+              required
+              defaultValue={loaderData.name}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password" className="text-zinc-500">
+              About:
+            </label>
+            <textarea className="dark:text-gray-50" name="about">
+              {loaderData.about}
+            </textarea>
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="password" className="text-zinc-500">
+              Phone:
+            </label>
+            <input
+              type="tel"
+              name="phone"
+              defaultValue={loaderData.phone}
+              className="dark:text-gray-50"
+            />
+          </div>
+          <div className="flex gap-2.5">
+            {isUpdate ? (
+              <button
+                type="submit"
+                className="mt-5 bg-blue-700 text-white flex-1 p-2 hover:cursor-pointer"
+              >
+                Update
+              </button>
+            ) : (
+              <button
+                type="submit"
+                className="mt-5 text-white flex-1 bg-gray-500 p-2 hover:cursor-pointer"
+                disabled
+              >
+                Update
+              </button>
+            )}
+            {isUpdate ? (
+              <button
+                type="button"
+                className={
+                  "mt-5 bg-blue-700 text-white flex-1 hover:cursor-pointer"
+                }
+                onClick={handleEdit}
+              >
+                Cancel
+              </button>
+            ) : (
+              <button
+                type="button"
+                className={
+                  "mt-5 bg-blue-700 text-white flex-1 hover:cursor-pointer"
+                }
+                onClick={handleEdit}
+              >
+                Edit
+              </button>
+            )}
+          </div>
+          {validations.length > 0 && (
+            <ul>
+              {validations.map((validation, index) => {
+                return (
+                  <li key={index} className="text-red-500 ">
+                    {validation}
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+          {result ? <p>{result}</p> : ""}
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
