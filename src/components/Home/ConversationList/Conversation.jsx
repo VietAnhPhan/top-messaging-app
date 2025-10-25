66c225b37ca0286828561d971e828fc28e5db820
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-10-25 15:38:22 +0700

    Implement style to display incomming messages are not seen yet

--- src/components/Home/ConversationList/Conversation.jsx
+++ src/components/Home/ConversationList/Conversation.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { UserContext } from "../../../Context";
 import api from "../../../api";
 import Avatar from "../../Avatar";
+import styles from "./Conversation.module.css";
 
 const Conversation = ({ conversation, userIds }) => {
   const userContext = useContext(UserContext);
@@ -43,16 +44,19 @@ const Conversation = ({ conversation, userIds }) => {
           <Avatar user={chatUser} type={"chatFrame"}></Avatar>
 
           <div className="">
-            <p className="text-base font-medium dark:text-slate-50">
+            <p className={`text-base font-medium dark:text-slate-50 ${!conversation.messages[0].isSeen && styles.userIncomingMessage}`}>
               {chatUser.name}
             </p>
-            <p className="text-sm dark:text-zinc-400">
+            <p className={`text-sm dark:text-zinc-400 ${!conversation.messages[0].isSeen && styles.incomingMessage}`}>
               {conversation.messages[0].message}
             </p>
           </div>
         </div>
 
-        <p className="dark:text-zinc-400 text-sm">{lastTimeFortmat}</p>
+        <div className="flex flex-col items-end">
+          <p className={`dark:text-zinc-400 text-sm ${!conversation.messages[0].isSeen && styles.dateIncomingMessage}`}>{lastTimeFortmat}</p>
+          {!conversation.messages[0].isSeen && (<span className={styles.circleIncomingMessage}></span>)}
+        </div>
       </div>
     </div>
   );
